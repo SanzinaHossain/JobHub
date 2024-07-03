666dc26d65ce5b1448905454822dfcdeda45cda8
SanzinaHossain <sanzina67hossain@gmail.com>
2024-07-03 15:07:03 +0600

    route set and job list create

---
+++ src/Components/Screens/Jobs/SingleJobList.jsx
@@ -0,0 +1,33 @@
+import locationImage from "../../../assets/icons/Location.png"
+import moneyImage from "../../../assets/icons/money1.png"
+import JobDetailsButton from "../../Molecules/JobDetailsButton/JobDetailsButton"
+import JobType from "../../Molecules/JobType/JobType"
+import TextWithIcon from "../../Molecules/TextWithIcon/TextWithIcon"
+import { Styles } from "./Styles"
+
+export default function SingleJobList({ job }) {
+  return (
+    <div className={Styles.singleJobListContainer}>
+      {/* .... image ... */}
+      <div className={Styles.imageContainer}>
+        <img src={job.logo} />
+      </div>
+
+      {/* job description  */}
+      <div>
+        <h1 className={Styles.titleStyle}>{job.job_title}</h1>
+        <h1 className={Styles.companyNameStyle}>{job.company_name}</h1>
+        <div className={Styles.locationContainer}>
+          <TextWithIcon image={locationImage} text={job.location} />
+          <TextWithIcon image={moneyImage} text={`Salary: ${job.salary}`} />
+        </div>
+        <JobType job={job} />
+      </div>
+
+      {/* details button  */}
+      <div>
+        <JobDetailsButton />
+      </div>
+    </div>
+  )
+}
---
+++ src/Components/Screens/Jobs/Styles.jsx
@@ -0,0 +1,12 @@
+export const Styles = {
+  container:
+    "flex flex-col justify-center items-center w-full bg-white py-10 gap-y-10 lg:px-0 px-5",
+
+  // single Job List component
+  singleJobListContainer:
+    "rounded-lg border-2 border-purple-100 lg:w-[850px] lg:h-[210px] shadow-md flex p-4 items-center justify-around flex-wrap lg:gap-y-0 gap-y-5",
+  imageContainer: "h-full w-52  bg-slate-100 flex justify-center items-center",
+  titleStyle: "text-xl text-black font-bold",
+  companyNameStyle: "text-lg text-black ",
+  locationContainer: "flex gap-x-5",
+}
