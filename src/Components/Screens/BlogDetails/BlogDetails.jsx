7ebf52a487ca270334b22feed8a8b49e4637ca83
SanzinaHossain <sanzina67hossain@gmail.com>
2024-07-04 01:10:12 +0600

    Blog Details

--- src/Components/Screens/BlogDetails/BlogDetails.jsx
+++ src/Components/Screens/BlogDetails/BlogDetails.jsx
@@ -2,20 +2,22 @@ import React from "react"
 import DynamicBanner from "../../Molecules/DynamicBanner/DynamicBanner"
 import { useLocation } from "react-router-dom"
 import blog3 from "../../../assets/images/blog3.jpg"
+import { Styles } from "./Styles"
+
 export default function BlogDetails() {
   const location = useLocation()
   const blogData = location.state.blog
   return (
     <div>
       <DynamicBanner title="Blog Information" />
-      <div className="w-full bg-white p-10 flex flex-col justify-center items-center">
-        <div className=" w-[800px]">
-          <h1 className="text-black text-xl mt-3">
+      <div className={Styles.container}>
+        <div className="lg:w-[800px] w-auto ">
+          <h1 className={Styles.dateStyle}>
             <strong>Featured - </strong> {blogData.date}
           </h1>
-          <h1 className="text-purple-950 text-4xl mt-3">{blogData.title} </h1>
+          <h1 className={Styles.titleStyle}>{blogData.title} </h1>
           <img className="mt-5" src={blogData.image} />
-          <h1 className="text-lg mt-5 text-black text-justify">
+          <h1 className={Styles.textStyle}>
             Tomfoolery crikey bits and bobs brilliant bamboozled down the pub
             amongst brolly hanky panky, cack bonnet arse over tit burke bugger
             all mate bodge. cillum dolore eu fugiat pariatur. Excepteur sint
@@ -23,16 +25,16 @@ export default function BlogDetails() {
             mollit anim id est laborum.Suspendisse interdum consectetur libero
             id faucibu nisl. Lacus vel facilisis volutpat est velit egestas.
           </h1>
-          <h1 className="text-lg mt-5 text-justify text-black">
+          <h1 className={Styles.textStyle}>
             Tempus imperdiet nulla malesuada pellentesque elit eget gravida cum.
             Sit amet ris nullam eget felis. Enim praesent elementum facilisis
             leo. Ultricies leo integer.
           </h1>
           <img className="mt-5" src={blog3} />
-          <h1 className="text-4xl text-black mt-5">
+          <h1 className={Styles.secondTitleStyle}>
             Work Harder & Gain Success
           </h1>
-          <h1 className="text-lg mt-5 text-black text-justify">
+          <h1 className={Styles.textStyle}>
             Tomfoolery crikey bits and bobs brilliant bamboozled down the pub
             amongst brolly hanky panky, cack bonnet arse over tit burke bugger
             all mate bodge. cillum dolore eu fugiat pariatur. Excepteur sint
---
+++ src/Components/Screens/BlogDetails/Styles.jsx
@@ -0,0 +1,7 @@
+export const Styles = {
+  container: "w-full bg-white p-10 flex flex-col justify-center items-center",
+  dateStyle: "text-black text-xl mt-3",
+  titleStyle: "text-purple-950 text-4xl mt-3",
+  textStyle: "text-lg mt-5 text-black text-justify",
+  secondTitleStyle: "text-4xl text-black mt-5",
+}
